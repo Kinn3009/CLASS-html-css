520534edfe389b4f79f16ba1e318d9c2f9067bbf
Kinn3009 <anhthiendn100@gmail.com>
2023-12-12 21:07:12 +0700

    code

--- REACTJS/CODE-reactjs/src/App.js
+++ REACTJS/CODE-reactjs/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Profile from "./components/Profile";
@@ -131,8 +131,114 @@ function App() {
       IsLightMode: true,
     },
   ];
+  const showMessage = (mes)=>{
+    console.log('kết quả là' ,mes );
+  }
+  
+  const sum = ( a , b , c , d , e , callback1, callback2) => {
+    callback1(a+b+c+d+e, callback2)
+  }
+  const divide = (num, callback2 ) => {
+    callback2(num / 5);
+  }
+
+  sum(1, 2, 3, 4, 5, divide, showMessage);
+
+  const [count, setCount] = useState(5);
+  const [tab, setTab] = useState(1);
+  // count: tên state
+  // setCount: callback func ghi mới giá trị cho state count
+  // 5: giá trị khởi tạo cho count
+
+  const onPlusCount = () => {
+    setCount((preState) => preState + 1); // Chính thống
+    setCount((preState) => preState + 1); // Chính thống
+    // setCount(count + 1)
+    // setCount(count + 1)
+  }
+
+  const onMinusCount = () => {
+    // setCount(count - 1);
+    setCount((preState) => preState - 1); // Chính thống
+  }
+
+  // const onChangeTab = () => {
+  // setTab(tab + 1)
+  // };
+
+
+
+
+  const onChangeTab = (currentTab) => () => {
+    setTab(currentTab);
+  };
+
+  const content = () => {
+    let content =  '';
+    switch (tab) {
+      case 1:
+        content = 'Tab 1 ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores'
+        break;
+      case 2:
+        content = 'Tab 2 ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores'
+        break;
+      case 3:
+        content = 'Tab 3 ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores'
+        break;
+      case 4:
+        content = 'Tab 4 ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores'
+        break;
+    
+      default:
+        content = '';
+        break;
+    }
+
+    return content;
+  }
+
+
+
+
+
+
+
+
   return (
     <>
+
+
+
+
+<div className='container'>
+        <div className='wrapper d-flex justify-content-center'>
+          {/* <button className={`btn-tab ${tab === 1 ? 'active' : ''}`} onClick={onChangeTab}>History</button> */}
+          <button className={`btn-tab ${tab === 1 ? 'active' : ''}`} onClick={onChangeTab(1)}>History</button>
+          <button className={`btn-tab ${tab === 2 ? 'active' : ''}`} onClick={onChangeTab(2)}>Method</button>
+          <button className={`btn-tab ${tab === 3 ? 'active' : ''}`} onClick={onChangeTab(3)}>Class</button>
+          <button className={`btn-tab ${tab === 4 ? 'active' : ''}`} onClick={onChangeTab(4)}>Student</button>
+        </div>
+
+        <div className='wrapper'>
+          <p>{content()}</p>
+        </div>
+      </div>
+
+
+
+
+
+
+    {/* <div className="App">
+      <h1 >Tuổi: {count}</h1>
+      <button onClick={onChangeCount}>Tăng</button>
+    </div> */}
+
+
+
+
+
+
       <div className="App">
         {/* {looping(10)} 
       <Profile/> */}
